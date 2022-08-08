f196dfc5a14b871410748abec414704f045ae242
Reema Almuhaimeed <rmrmhm23@gmail.com>
2022-08-08 12:42:49 +0300

    first commit

---
+++ sample-node-js-app/Objects and the DOM/home.js
@@ -0,0 +1,20 @@
+const header = document.getElementById('message');
+
+header.style.fontWeight = '100';
+
+header.style.color = 'blue';
+
+const button = document.getElementById("see-review");
+
+button.addEventListener('click',function(){
+    const review = document.getElementById('review');
+
+    if(review.classList.contains('d-none')){
+        review.classList.remove('d-none');
+        button.textContent ='CLOSE REVIEW';
+    }else{
+        review.classList.add('d-none');
+        button.textContent ='SEE REVIEW';
+    }  
+
+});
